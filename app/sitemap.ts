5136c06b3ec19dfd4600ad344c1e8efc85acdf95
Bigboarddc <bigboarddc@gmail.com>
2026-02-12 20:09:16 -0500

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,19 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://bigboarddc.com',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://bigboarddc.com/menu',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+  ]
+}
 
